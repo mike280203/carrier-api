cb54ea44d7a7f0d944650e1a1dc7ee767fbcf676
JM01332 <jemeie@web.de>
2026-04-05 13:39:12 +0200

    Add dataclass-aware entity base

--- src/carrier/Entity/base.py
+++ src/carrier/Entity/base.py
@@ -1,7 +1,20 @@
 """Basisklasse für Entity-Klassen."""
 
+from typing import TYPE_CHECKING, Any
+
 from sqlalchemy.orm import DeclarativeBase
 
+if TYPE_CHECKING:
 
-class Base(DeclarativeBase):
-    """Basisklasse für alle Entities."""
+    class MappedAsDataclass:
+        """Mixin-Klasse, ohne die Directiven von PEP 681 zu verwenden."""
+
+        def __init__(self, *arg: Any, **kw: Any) -> None:
+            """Mixin-Klasse, ohne die Directiven von PEP 681 zu verwenden."""
+
+else:
+    from sqlalchemy.orm import MappedAsDataclass
+
+
+class Base(MappedAsDataclass, DeclarativeBase):
+    """Basisklasse für Entity-Klassen als Dataclass."""
