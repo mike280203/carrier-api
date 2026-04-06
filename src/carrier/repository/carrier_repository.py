c7b930d9b309b077d8ce24a15b7e110665f066a6
mike280203 <mikesteidle2802@gmail.com>
2026-04-07 00:25:02 +0200

    repo methoden fertig

--- src/carrier/repository/carrier_repository.py
+++ src/carrier/repository/carrier_repository.py
@@ -1,6 +1,6 @@
 """Repository fuer persistente Carrierdaten."""
 
-from collections.abc import Mapping, Sequence
+from collections.abc import Mapping
 from typing import Final
 
 from loguru import logger
@@ -128,3 +128,86 @@ class CarrierRepository:
             Carrier).filter(Carrier.carrier_type == carrier_type)
         count: Final = session.execute(statement).scalar()
         return count if count is not None else 0
+
+    def exists_name(self, name: str, session: Session) -> bool:
+        """Gibt es einen Carrier mit dem namen schon."""
+        logger.debug("name={}", name)
+        statement: Final = select(func.count()).where(Carrier.name == name)
+        anzahl: Final = session.scalar(statement)
+        logger.debug("anzahl={}", anzahl)
+        return anzahl is not None and anzahl > 0
+
+    def find(
+        self,
+        suchparameter: Mapping[str, str],
+        pageable: Pageable,
+        session: Session,
+    ) -> Slice[Carrier]:
+        """Suche mit Suchparameter."""
+        log_str: Final = "{}"
+        logger.debug(log_str, suchparameter)
+        if not suchparameter:
+            return self._find_all(pageable=pageable, session=session)
+
+        # Iteration ueber die Schluessel des Dictionaries mit den Suchparameter
+        for key, value in suchparameter.items():
+            if key == "name":
+                carrier = self._find_by_name(name=value, session=session)
+                logger.debug(log_str, carrier)
+                return (
+                    Slice(content=(carrier,), total_elements=1)
+                    if carrier is not None
+                    else Slice(content=(), total_elements=0)
+                )
+            if key == "nation":
+                carriers = self._find_by_nation(
+                    teil=value, pageable=pageable, session=session
+                )
+                logger.debug(log_str, carriers)
+                return carriers
+            if key == "carrier_type":
+                try:
+                    carrier_type_enum = CarrierType(value)
+                except ValueError:
+                    return Slice(content=(), total_elements=0)
+
+                carriers = self._find_by_carrier_type(
+                    carrier_type_enum, pageable=pageable, session=session
+                )
+                logger.debug(log_str, carriers)
+                return carriers
+        return Slice(content=(), total_elements=0)
+
+    def create(self, carrier: Carrier, session: Session) -> Carrier:
+        """Speichere einen neuen Carrier ab."""
+        logger.debug(
+            "carrier={}, carrier.commandcenter={}, carrier.aircrafts={}",
+            carrier,
+            carrier.commandcenter,
+            carrier.aircrafts,
+        )
+        session.add(instance=carrier)
+        session.flush(objects=[carrier])
+        logger.debug("carrier_id={}", carrier.id)
+        return carrier
+
+    def update(self, carrier: Carrier, session: Session) -> Carrier | None:
+        """Aktualisiere einen Carrier."""
+        logger.debug("{}", carrier)
+
+        if (carrier_db := self.find_by_id(
+            carrier_id=carrier.id, session=session)) is None:
+            return None
+        carrier_db.set(carrier)
+
+        logger.debug("{}", carrier_db)
+        return carrier_db
+
+    def delete_by_id(self, carrier_id: int, session: Session) -> None:
+        """Lösche die Daten zu einem Carrier."""
+        logger.debug("carrier_id={}", carrier_id)
+
+        if (carrier := self.find_by_id(carrier_id=carrier_id, session=session)) is None:
+            return
+        session.delete(carrier)
+        logger.debug("ok")
