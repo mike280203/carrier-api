f47da395ea7e3400b75e93dc233d1c91050ccd35
mike280203 <mikesteidle2802@gmail.com>
2026-04-06 19:12:41 +0200

    find_all + hilfsmethoden (test fehlt)

--- src/carrier/repository/carrier_repository.py
+++ src/carrier/repository/carrier_repository.py
@@ -8,6 +8,8 @@ from sqlalchemy import func, select
 from sqlalchemy.orm import Session, joinedload
 
 from carrier.entity.carrier import Carrier
+from carrier.repository.pageable import Pageable
+from carrier.repository.slice import Slice
 
 __all__ = ["CarrierRepository"]
 
@@ -31,3 +33,30 @@ class CarrierRepository:
 
         logger.debug("{}", carrier)
         return carrier
+
+    def find_all(self, pageable: Pageable, session: Session) -> Slice[Carrier]:
+        """Methode zum suchen aller Carrier."""
+        logger.debug("aufgerufen")
+        offset = pageable.number * pageable.size
+
+        if pageable.size != 0:
+            statement: Final = (
+                select(Carrier)
+                .options(joinedload(Carrier.commandcenter))
+                .limit(pageable.size)
+                .offset(offset)
+            )
+        else:
+            statement: Final = select(Carrier).options(
+                joinedload(Carrier.commandcenter))
+
+        carriers: Final = session.scalars(statement).all()
+        anzahl: Final = self._count_all_rows(session)
+        carrier_slice: Final = Slice(content=tuple(carriers), total_elements=anzahl)
+        logger.debug("carrier_slice={}", carrier_slice)
+        return carrier_slice
+
+    def _count_all_rows(self, session: Session) -> int:
+        statement: Final = select(func.count()).select_from(Carrier)
+        count: Final = session.execute(statement).scalar()
+        return count if count is not None else 0
